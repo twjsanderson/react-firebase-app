a315fcbbb59d4f850b335c6a45baa534f3ad36f8
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2019-11-26 23:14:55 -0500

    added logo image and assets folder to public, add handleCreateEvent function

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Re-vents</h1>
-        <button className="ui icon button">
-          <i className="smile icon"></i>
-          CSS Button
-        </button>
-        <Button
-          icon="smile"
-          content="React Button"  
-        />
-      </div>
+      <>
+      <NavBar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container>
+      </>
     );
   }
 }
