b5b69c8806ab1d44218cdcde9be0530a989e08b1
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2019-11-26 20:00:49 -0500

    first commit, basic buttons

---
+++ src/app/layout/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Re-vents</h1>
+        <button className="ui icon button">
+          <i className="smile icon"></i>
+          CSS Button
+        </button>
+        <Button
+          icon="smile"
+          content="React Button"  
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// The following code gives us hot reload that does not re-render the entire application
+// we access and modiify webpack below at module
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App/>, rootEl)
+}
+
+if (module.hot) {
+    module.hot.accept(App, () => {
+        setTimeout(render);
+    });
+};
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// JSX is an abstraction away from this:
+// React.createElement('div', {className: 'app'}),
+//  React.createElement('h1', null, 'Hello!')
+//  to this ->
+// <div className='app'>
+//   <h1>Hello</h1>
+//  </div>
